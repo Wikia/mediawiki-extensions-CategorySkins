0d0d030dc52b11c8e6ffedc466da8064302c4033
Tim Aldridge <taldridge@curse.com>
2015-07-07 09:36:09 -0500

    Moved keyup to it's own event. Added style removal on focus out.

--- js/categoryskins.js
+++ js/categoryskins.js
@@ -4,6 +4,10 @@ function CategorySkins($) {
 	this.init = function() {
 		$('.cs_prefix').on('focus', skins.highlightPrefix);
 		$('.cs_suffix').on('focus', skins.highlightSuffix);
+		$('.cs_prefix').on('focusout', skins.removePrefixBold);
+		$('.cs_suffix').on('focusout', skins.removeSuffixBold);
+		$('.cs_prefix input').on('keyup', skins.updatePrefix);
+		$('.cs_suffix input').on('keyup', skins.updateSuffix);
 
 
 		var prefix = $('.cs_prefix input').val();
@@ -14,19 +18,32 @@ function CategorySkins($) {
 
 	var skins = {
 		highlightPrefix: function() {
-			$('#skin_suffix').removeAttr('style');
 			$('#skin_prefix').css('font-weight', 'bold');
+
+		},
+
+		highlightSuffix: function() {
+			$('#skin_suffix').css('font-weight', 'bold');
+		},
+
+		updatePrefix: function() {
 			$(this).keyup(function() {
 				$('#skin_prefix').html($(this).val());
 			});
 		},
 
-		highlightSuffix: function() {
-			$('#skin_prefix').removeAttr('style');
-			$('#skin_suffix').css('font-weight', 'bold');
+		updateSuffix: function() {
 			$(this).keyup(function() {
 				$('#skin_suffix').html($(this).val());
 			});
+		},
+
+		removePrefixBold: function() {
+			$('#skin_prefix').removeAttr('style');
+		},
+
+		removeSuffixBold: function() {
+			$('#skin_suffix').removeAttr('style');
 		}
 	};
 }
