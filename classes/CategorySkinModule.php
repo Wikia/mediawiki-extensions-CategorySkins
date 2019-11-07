cd1d311d9edb89f00fd4bcfb0f1fd8da69ef8f2e
Alexia E. Smith <washuu@gmail.com>
2019-11-07 15:05:10 -0600

    Code standards.

--- classes/CategorySkinModule.php
+++ classes/CategorySkinModule.php
@@ -4,25 +4,24 @@
  * Category Skins
  * Apply custom styles to pages according to category membership
  *
- * @author		Noah Manneschmidt
- * @copyright	(c) 2015 Curse Inc.
- * @license		GNU General Public License v2.0 or later
- * @package		CategorySkins
- * @link		https://gitlab.com/hydrawiki
- *
+ * @author    Noah Manneschmidt
+ * @copyright (c) 2015 Curse Inc.
+ * @license   GPL-2.0-or-later
+ * @package   CategorySkins
+ * @link      https://gitlab.com/hydrawiki
 **/
 
 class CategorySkinModule extends ResourceLoaderWikiModule {
 	/**
 	 * Gets list of pages used by this module
 	 *
-	 * @param $context ResourceLoaderContext
+	 * @param  $context ResourceLoaderContext
 	 * @return Array: List of pages
 	 */
-	protected function getPages( \ResourceLoaderContext $context ) {
-		//Drop first 23 characters. (ext.categoryskins.skin.)
+	protected function getPages(\ResourceLoaderContext $context) {
+		// Drop first 23 characters. (ext.categoryskins.skin.)
 		return [
-			'MediaWiki:'.substr($this->name, 23).'.css' => [
+			'MediaWiki:' . substr($this->name, 23) . '.css' => [
 				'type' => 'style'
 			]
 		];
@@ -30,7 +29,8 @@ class CategorySkinModule extends ResourceLoaderWikiModule {
 
 	/**
 	 * Gets group name
-	 * @return	string	Name of group
+	 *
+	 * @return string	Name of group
 	 */
 	public function getGroup() {
 		return 'site';
@@ -38,6 +38,7 @@ class CategorySkinModule extends ResourceLoaderWikiModule {
 
 	/**
 	 * Get position
+	 *
 	 * @return string	Position for the css
 	 */
 	public function getPosition() {
